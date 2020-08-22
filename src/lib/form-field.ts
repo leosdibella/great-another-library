cdf0338944b478f4fe679e04e6fb1d6d5ecc2e39
Leonardo DiBella <leosdibella@gmail.com>
2020-08-22 10:45:21 -0400

    1) Forgot to add the husky git hook with prettier earlier 2) Minor adjustments for clarity in GalCustomElement 3) Fixing spacing issues (VS Code defaults to 4 spaces)

--- src/lib/form-field.ts
+++ src/lib/form-field.ts
@@ -1,189 +1,235 @@
-import { GalCustomElement, doNothing } from "./utilities";
+import { GalCustomElement, doNothing } from './utilities';
 
 const styles = `
 <style>
-    :host {
+  :host {
 
-    }
+  }
 
-    ::slotted(label) {
-        font-weight: bold;
-    }
+  ::slotted(label) {
+    font-weight: bold;
+  }
 
-    ::slotted(input) {
-        border: none;
-        background-color: transparent;
-        border-radius: 0;
-        flex: 1 0 auto;
-    }
+  ::slotted(input) {
+    border: none;
+    background-color: transparent;
+    border-radius: 0;
+    flex: 1 0 auto;
+  }
 
-    ::slotted(input:focus) {
-        outline: none;
-    }
+  ::slotted(input:focus) {
+    outline: none;
+  }
 
-    ::slotted(i) {
-        background-color: transparent;
-    }
+  ::slotted(i) {
+    background-color: transparent;
+  }
 
-    .gal-form-field-container {
-        border: 0.0125rem solid #000000;
-        border-radius: 0.025rem;
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-    }
+  .gal-form-field-container {
+    border: 0.0125rem solid #000000;
+    border-radius: 0.025rem;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
 
-    .gal-form-field-container-focused {
-        outline: 0.0125rem solid #000000;
-    }
+  .gal-form-field-container-focused {
+    outline: 0.0125rem solid #000000;
+  }
 </style>
 `;
 
 const html = `
-    <slot name="label">
-    </slot>
-    <div
-        class="gal-form-field-container">
-        <slot name="prefix">
-        </slot>
-        <slot name="input">
-        </slot>
-        <slot name="suffix">
-        </slot>
-    </div>
-    <div>
-        <slot name="description">
-        </slot>
-    </div>
-    <div>
-        <slot name="message">
-        </slot>
-    </div>
+<slot name="label">
+</slot>
+<div
+  class="gal-form-field-container">
+  <slot name="prefix">
+  </slot>
+  <slot name="input">
+  </slot>
+  <slot name="suffix">
+  </slot>
+</div>
+<div>
+  <slot name="description">
+  </slot>
+</div>
+<div>
+  <slot name="message">
+  </slot>
+</div>
 `;
 
 enum Position {
-    single = 0,
-    prefixed = 1 << 0,
-    suffixed = 1 << 1,
-    all = prefixed | suffixed
+  single = 0,
+  prefixed = 1 << 0,
+  suffixed = 1 << 1,
+  all = prefixed | suffixed,
 }
 
 @GalCustomElement<GalFormField>({
-    styles,
-    html,
-    tag: 'gal-form-field',
-    observedAttributes: {
-      'font-size': ''
+  styles,
+  html,
+  tag: 'gal-form-field',
+  observedAttributes: {
+    'font-size': '',
+  },
+  observedAttributesMapName: 'attributeMap',
+})
+export class GalFormField extends HTMLElement {
+  private static nextId = 0;
+  private static readonly remToPixels = 16;
+  private static readonly verticalPaddingModifier = 0.5;
+  private static readonly horizontalPaddingModifier = 0.75;
+
+  #nextId = ++GalFormField.nextId;
+  #label?: HTMLLabelElement;
+  #input?: HTMLInputElement;
+  #suffix?: HTMLElement;
+  #prefix?: HTMLElement;
+  #fontSize: string = '1rem';
+  #formFieldContainer!: HTMLDivElement;
+
+  #fontSizeMap: Readonly<
+    Record<
+      Position,
+      (
+        verticalPadding: number,
+        horizontalPadding: number,
+        fontSize: number,
+      ) => void
+    >
+  > = {
+    [Position.single]: doNothing,
+    [Position.prefixed]: (
+      verticalPadding: number,
+      horizontalPadding: number,
+      fontSize: number,
+    ) => {
+      this.#prefix!.style.padding = `${verticalPadding}rem 0 ${verticalPadding}rem ${horizontalPadding}rem`;
+      this.#prefix!.style.fontSize = `${fontSize}rem`;
     },
-    observedAttributesMapName: 'attributeMap'
-  })
-  export class GalFormField extends HTMLElement {
-    private static nextId = 0;
-    private static readonly remToPixels = 16;
-    private static readonly verticalPaddingModifier = 0.5;
-    private static readonly horizontalPaddingModifier = 0.75;
-        
-    #nextId = ++GalFormField.nextId;
-    #label?: HTMLLabelElement;
-    #input?: HTMLInputElement;
-    #suffix?: HTMLElement;
-    #prefix?: HTMLElement;
-    #fontSize: string = '1rem';
-    #formFieldContainer!: HTMLDivElement;
+    [Position.suffixed]: (
+      verticalPadding: number,
+      horizontalPadding: number,
+      fontSize: number,
+    ) => {
+      this.#suffix!.style.padding = `${verticalPadding}rem ${horizontalPadding}rem ${verticalPadding}rem 0`;
+      this.#suffix!.style.fontSize = `${fontSize}rem`;
+    },
+    [Position.all]: (
+      verticalPadding: number,
+      horizontalPadding: number,
+      fontSize: number,
+    ) => {
+      this.#fontSizeMap[Position.prefixed](
+        verticalPadding,
+        horizontalPadding,
+        fontSize,
+      );
+      this.#fontSizeMap[Position.suffixed](
+        verticalPadding,
+        horizontalPadding,
+        fontSize,
+      );
+    },
+  };
 
-    #fontSizeMap: Readonly<Record<Position, (verticalPadding: number, horizontalPadding: number, fontSize: number) => void>> = {
-        [Position.single]: doNothing,
-        [Position.prefixed]: (verticalPadding: number, horizontalPadding: number, fontSize: number) => {
-            this.#prefix!.style.padding = `${verticalPadding}rem 0 ${verticalPadding}rem ${horizontalPadding}rem`;
-            this.#prefix!.style.fontSize = `${fontSize}rem`;
-        },
-        [Position.suffixed]: (verticalPadding: number, horizontalPadding: number, fontSize: number) => {
-            this.#suffix!.style.padding = `${verticalPadding}rem ${horizontalPadding}rem ${verticalPadding}rem 0`;
-            this.#suffix!.style.fontSize = `${fontSize}rem`;
-        },
-        [Position.all]: (verticalPadding: number, horizontalPadding: number, fontSize: number) => {
-            this.#fontSizeMap[Position.prefixed](verticalPadding, horizontalPadding, fontSize);
-            this.#fontSizeMap[Position.suffixed](verticalPadding, horizontalPadding, fontSize);
-        }
-    };
+  #focus = () => {
+    this.#formFieldContainer.classList.add('gal-form-field-container-focused');
+  };
 
-    #focus = () => {
-        this.#formFieldContainer.classList.add('gal-form-field-container-focused');
-    };
+  #blur = () => {
+    this.#formFieldContainer.classList.remove(
+      'gal-form-field-container-focused',
+    );
+  };
 
-    #blur = () => {
-        this.#formFieldContainer.classList.remove('gal-form-field-container-focused');
-    };
+  private resizeFormField() {
+    const fontSizeInRem =
+      (parseFloat(getComputedStyle(document.body).fontSize) /
+        GalFormField.remToPixels) *
+      parseFloat(this.#fontSize);
+    const verticalPadding =
+      fontSizeInRem * GalFormField.verticalPaddingModifier;
+    const horizontalPadding =
+      fontSizeInRem * GalFormField.horizontalPaddingModifier;
 
-    private resizeFormField() {
-        const fontSizeInRem = parseFloat(getComputedStyle(document.body).fontSize) / GalFormField.remToPixels * parseFloat(this.#fontSize);
-        const verticalPadding = fontSizeInRem * GalFormField.verticalPaddingModifier;
-        const horizontalPadding = fontSizeInRem * GalFormField.horizontalPaddingModifier;
-        
-        let position: Position = Position.single;
+    let position: Position = Position.single;
 
-        if (this.#prefix) {
-            position |= Position.prefixed;
-        }
-
-        if (this.#suffix) {
-            position |= Position.suffixed;
-        }
-
-        if (this.#input) {
-            this.#input.style.padding = `${verticalPadding}rem ${horizontalPadding}rem`;
-            this.#input.style.fontSize = `${fontSizeInRem}rem`;
-        }
-
-        this.#fontSizeMap[position](verticalPadding, horizontalPadding, fontSizeInRem);
+    if (this.#prefix) {
+      position |= Position.prefixed;
     }
 
-    public attributeMap: Readonly<
-        Partial<Record<keyof GalFormField, (from: string, to: string) => void>>
-    > = {
-        fontSize: (from: string, to: string) => {
-            this.fontSize = to;
-        }
-    };
-
-    public get fontSize() {
-        return this.#fontSize;
+    if (this.#suffix) {
+      position |= Position.suffixed;
     }
 
-    public set fontSize(fontSize: string) {
-        if (this.#fontSize !== fontSize) {
-            this.#fontSize = fontSize;
-            this.resizeFormField();
-        }
+    if (this.#input) {
+      this.#input.style.padding = `${verticalPadding}rem ${horizontalPadding}rem`;
+      this.#input.style.fontSize = `${fontSizeInRem}rem`;
     }
 
-    disconnectedCallback() {
-        if (this.#input) {
-            this.#input.removeEventListener('focus', this.#focus);
-            this.#input.removeEventListener('blur', this.#blur);
-        }
-    }
+    this.#fontSizeMap[position](
+      verticalPadding,
+      horizontalPadding,
+      fontSizeInRem,
+    );
+  }
 
-    connectedCallback() {
-        if (this.shadowRoot) {
-            this.#formFieldContainer = this.shadowRoot.querySelector('.gal-form-field-container') as HTMLDivElement;
-        }
-        
-        this.#label = this.querySelector<HTMLLabelElement>('[slot="label"]') || undefined;
-        this.#input = this.querySelector<HTMLInputElement>('[slot="input"]') || undefined;
-        this.#prefix = this.querySelector<HTMLInputElement>('[slot="prefix"]') || undefined;
-        this.#suffix = this.querySelector<HTMLInputElement>('[slot="suffix"]') || undefined;
+  public attributeMap: Readonly<
+    Partial<Record<keyof GalFormField, (from: string, to: string) => void>>
+  > = {
+    fontSize: (from: string, to: string) => {
+      this.fontSize = to;
+    },
+  };
 
-        if (this.#label) {
-            this.#label.htmlFor = `gal-form-field-input-${this.#nextId}`;
-        }
+  public get fontSize() {
+    return this.#fontSize;
+  }
 
-        if (this.#input) {
-            this.#input.id = `gal-form-field-input-${this.#nextId}`;
-            this.#input.addEventListener('focus', this.#focus);
-            this.#input.addEventListener('blur', this.#blur);
-        }
-
-        this.resizeFormField();
+  public set fontSize(fontSize: string) {
+    if (this.#fontSize !== fontSize) {
+      this.#fontSize = fontSize;
+      this.resizeFormField();
     }
   }
+
+  disconnectedCallback() {
+    if (this.#input) {
+      this.#input.removeEventListener('focus', this.#focus);
+      this.#input.removeEventListener('blur', this.#blur);
+    }
+  }
+
+  connectedCallback() {
+    if (this.shadowRoot) {
+      this.#formFieldContainer = this.shadowRoot.querySelector(
+        '.gal-form-field-container',
+      ) as HTMLDivElement;
+    }
+
+    this.#label =
+      this.querySelector<HTMLLabelElement>('[slot="label"]') || undefined;
+    this.#input =
+      this.querySelector<HTMLInputElement>('[slot="input"]') || undefined;
+    this.#prefix =
+      this.querySelector<HTMLInputElement>('[slot="prefix"]') || undefined;
+    this.#suffix =
+      this.querySelector<HTMLInputElement>('[slot="suffix"]') || undefined;
+
+    if (this.#label) {
+      this.#label.htmlFor = `gal-form-field-input-${this.#nextId}`;
+    }
+
+    if (this.#input) {
+      this.#input.id = `gal-form-field-input-${this.#nextId}`;
+      this.#input.addEventListener('focus', this.#focus);
+      this.#input.addEventListener('blur', this.#blur);
+    }
+
+    this.resizeFormField();
+  }
+}
--- src/lib/utilities/gal-custom-element.ts
+++ src/lib/utilities/gal-custom-element.ts
@@ -1,4 +1,4 @@
-import { isNonEmptyString, kebabCaseToCamelCase } from "./utilities";
+import { isNonEmptyString, kebabCaseToCamelCase } from './utilities';
 
 interface IGalCustomElement<T> {
   tag: string;
@@ -18,8 +18,8 @@ interface IGalExtendedCustomElement<T> {
 }
 
 interface IGalEvent {
-  eventName: string,
-  eventFunctionName: string,
+  eventName: string;
+  eventFunctionName: string;
   querySelector: string;
   querySelectorIndex: number;
 }
@@ -35,38 +35,38 @@ const galEventRegex = /gal-on_[a-zA-Z0-9]+/g;
 const galDomParser = (function () {
   const domParser = new DOMParser();
 
-  return function(html: string): HTMLCollection | undefined {
-    const body = domParser.parseFromString(html, 'text/html').querySelector('body') || undefined;
+  return function (html: string): HTMLCollection | undefined {
+    const body =
+      domParser.parseFromString(html, 'text/html').querySelector('body') ||
+      undefined;
 
     return body ? body.children : undefined;
-  }
-}());
+  };
+})();
 
 function htmlCollectionToArray(htmlCollection: HTMLCollection) {
   return Object.keys(htmlCollection)
-               .map(k => htmlCollection.item(+k))
-               .filter(e => e instanceof Element) as Element[];
+    .map((k) => htmlCollection.item(+k))
+    .filter((e) => e instanceof Element) as Element[];
 }
 
-function getObservedAttributes<T>(preObservedAttributesMap?: Record<string, keyof T | ''>) {
-  const observedAttributes: string[] = [];
+function getObservedAttributes<T>(
+  preObservedAttributesMap: Record<string, keyof T | ''>,
+) {
+  const observedAttributes: string[] = Object.keys(preObservedAttributesMap);
   const observedAttributesMap: Record<string, keyof T> = {};
 
-  if (preObservedAttributesMap) {
-    const keys = Object.keys(preObservedAttributesMap);
+  for (let i = 0; i < observedAttributes.length; ++i) {
+    const attribute = observedAttributes[i];
+    const classProperty = (preObservedAttributesMap[attribute] ||
+      kebabCaseToCamelCase(attribute)) as keyof T;
 
-    for (let i = 0; i < keys.length; ++i) {
-      const key = keys[i];
-      const attributeName = (preObservedAttributesMap[key] || kebabCaseToCamelCase(key)) as keyof T;
-
-      observedAttributes.push(key);
-      observedAttributesMap[key] = attributeName;
-    }  
+    observedAttributesMap[attribute] = classProperty;
   }
 
   return {
     observedAttributes,
-    observedAttributesMap
+    observedAttributesMap,
   };
 }
 
@@ -79,7 +79,7 @@ function parseGalHtml(html: string): IGalParsedHtml | undefined {
 
   const parsedHtml: IGalParsedHtml = {
     template: html,
-    events: []
+    events: [],
   };
 
   const stack: Element[] = htmlCollectionToArray(htmlCollection);
@@ -89,7 +89,7 @@ function parseGalHtml(html: string): IGalParsedHtml | undefined {
     const element = stack.pop() as Element;
     const attributes = element.attributes;
     const keys = Object.keys(attributes);
-    
+
     for (let i = 0; i < keys.length; ++i) {
       if (attributes[i].name.match(galEventRegex)) {
         const eventFunctionName = attributes[i].nodeValue || '';
@@ -103,9 +103,12 @@ function parseGalHtml(html: string): IGalParsedHtml | undefined {
 
         parsedHtml.events.push({
           eventFunctionName,
-          eventName: attributes[i].name.substring(galEventPrefix.length, attributes[i].name.length),
+          eventName: attributes[i].name.substring(
+            galEventPrefix.length,
+            attributes[i].name.length,
+          ),
           querySelector,
-          querySelectorIndex: querySelectorIndices[querySelector] as number
+          querySelectorIndex: querySelectorIndices[querySelector] as number,
         });
       }
     }
@@ -114,7 +117,7 @@ function parseGalHtml(html: string): IGalParsedHtml | undefined {
       continue;
     }
 
-    htmlCollectionToArray(element.children).forEach(e => stack.push(e));
+    htmlCollectionToArray(element.children).forEach((e) => stack.push(e));
   }
 
   return parsedHtml;
@@ -125,39 +128,46 @@ export interface IGalCustomElementDefinition {
 }
 
 export function GalCustomElement<T>(galCustomElement: IGalCustomElement<T>) {
-  return function<S extends CustomElementConstructor>(customElement: S) {
+  return function <S extends CustomElementConstructor>(customElement: S) {
     const defaultDocument: Document = galCustomElement.document || document;
     const template = defaultDocument.createElement('template');
 
-    const {
-      observedAttributes,
-      observedAttributesMap
-    } = getObservedAttributes<T>(galCustomElement.observedAttributes);
+    const { observedAttributes, observedAttributesMap } = getObservedAttributes<
+      T
+    >(galCustomElement.observedAttributes || {});
 
     if (!isNonEmptyString(galCustomElement.tag)) {
-      throw new Error('GalCustomElement constructor error: tag cannot be empty!');
+      throw new Error(
+        'GalCustomElement constructor error: tag cannot be empty!',
+      );
     }
 
     const templateId = `${galCustomElement.tag}-template`;
 
     if (customElements.get(galCustomElement.tag)) {
-      throw new Error('GalCustomElement constructor error: duplicate custom element tag detected!');
+      throw new Error(
+        'GalCustomElement constructor error: duplicate custom element tag detected!',
+      );
     }
 
     const parsedHtml = parseGalHtml(galCustomElement.html);
 
     if (!parsedHtml) {
-      throw new Error('GalCustomElement constructor error: unable to parse valid html from template!');
+      throw new Error(
+        'GalCustomElement constructor error: unable to parse valid html from template!',
+      );
     }
 
     template.setAttribute('id', templateId);
-    template.innerHTML = `${galCustomElement.styles || ''}${parsedHtml.template}`;
+    template.innerHTML = `${galCustomElement.styles || ''}${
+      parsedHtml.template
+    }`;
 
     const galCustomElementDefintion = class GalCustomElementDefinition extends customElement {
       public static get observedAttributes() {
         return observedAttributes;
       }
-      
+
       public get document() {
         return defaultDocument;
       }
@@ -172,46 +182,63 @@ export function GalCustomElement<T>(galCustomElement: IGalCustomElement<T>) {
 
       public attributeChangedCallback(name: string, from: string, to: string) {
         if (!galCustomElement.observedAttributesMapName) {
-          throw new Error('GalCustomElement attributeChangedCallback error: no observed attributes handler map name detected!');
+          throw new Error(
+            'GalCustomElement attributeChangedCallback error: no observed attributes handler map name detected!',
+          );
         }
 
         if (from === to) {
           return;
         }
 
-        const attributesMap = this[galCustomElement.observedAttributesMapName as keyof this];
+        const attributesMap = this[
+          galCustomElement.observedAttributesMapName as keyof this
+        ];
 
         if (typeof attributesMap !== 'object' || attributesMap === null) {
-          throw new Error('GalCustomElement attributeChangedCallback error: no observed attributes handler map object detected!');
+          throw new Error(
+            'GalCustomElement attributeChangedCallback error: no observed attributes handler map object detected!',
+          );
         }
-    
-        const attributeChangeHandler = (attributesMap as Partial<Record<keyof T, (from: string, to: string) => void>>)[observedAttributesMap[name]];
-    
+
+        const attributeChangeHandler = (attributesMap as Partial<
+          Record<keyof T, (from: string, to: string) => void>
+        >)[observedAttributesMap[name]];
+
         if (attributeChangeHandler) {
           attributeChangeHandler(from, to);
         }
       }
-    
+
       constructor(...args: any[]) {
         super();
-        
-        this.attachShadow({ mode: 'open' }).appendChild((template).content.cloneNode(true));
+
+        this.attachShadow({ mode: 'open' }).appendChild(
+          template.content.cloneNode(true),
+        );
         const events = (parsedHtml as IGalParsedHtml).events;
 
         for (let i = 0; i < events.length; ++i) {
-          const event = (this[events[i].eventFunctionName as keyof this]) as (this[keyof this] & ((event: Event) => void));
+          const event = this[
+            events[i].eventFunctionName as keyof this
+          ] as this[keyof this] & ((event: Event) => void);
 
           if (typeof event !== 'function' || !this.shadowRoot) {
             continue;
           }
-          
-          const eventBoundElement = this.shadowRoot.querySelectorAll(events[i].querySelector)[events[i].querySelectorIndex];
+
+          const eventBoundElement = this.shadowRoot.querySelectorAll(
+            events[i].querySelector,
+          )[events[i].querySelectorIndex];
 
           if (!eventBoundElement) {
             continue;
           }
-          
-          eventBoundElement.addEventListener(events[i].eventName, event.bind(this));
+
+          eventBoundElement.addEventListener(
+            events[i].eventName,
+            event.bind(this),
+          );
         }
       }
     };
@@ -220,17 +247,19 @@ export function GalCustomElement<T>(galCustomElement: IGalCustomElement<T>) {
     defaultDocument.body.appendChild(template);
 
     return galCustomElementDefintion;
-  }
+  };
 }
 
-export function GalExtendedCustomElement<T>(galExtendedCustomElement: IGalExtendedCustomElement<T>) {
-  return function<S extends CustomElementConstructor>(customElement: S) {
-    const defaultDocument: Document = galExtendedCustomElement.document || document;
-    
-    const {
-      observedAttributes,
-      observedAttributesMap
-    } = getObservedAttributes<T>(galExtendedCustomElement.observedAttributes);
+export function GalExtendedCustomElement<T>(
+  galExtendedCustomElement: IGalExtendedCustomElement<T>,
+) {
+  return function <S extends CustomElementConstructor>(customElement: S) {
+    const defaultDocument: Document =
+      galExtendedCustomElement.document || document;
+
+    const { observedAttributes, observedAttributesMap } = getObservedAttributes<
+      T
+    >(galExtendedCustomElement.observedAttributes || {});
 
     const galExtendedCustomElementDefintion = class GalExtendedCustomElementDefintion extends customElement {
       public static get observedAttributes() {
@@ -243,21 +272,29 @@ export function GalExtendedCustomElement<T>(galExtendedCustomElement: IGalExtend
 
       public attributeChangedCallback(name: string, from: string, to: string) {
         if (!galExtendedCustomElement.observedAttributesMapName) {
-          throw new Error('GalExtendedCustomElement attributeChangedCallback error: no observed attributes handler map name detected!');
+          throw new Error(
+            'GalExtendedCustomElement attributeChangedCallback error: no observed attributes handler map name detected!',
+          );
         }
 
         if (from === to) {
           return;
         }
 
-        const attributesMap = this[galExtendedCustomElement.observedAttributesMapName as keyof this];
+        const attributesMap = this[
+          galExtendedCustomElement.observedAttributesMapName as keyof this
+        ];
 
         if (typeof attributesMap !== 'object' || attributesMap === null) {
-          throw new Error('GalExtendedCustomElement attributeChangedCallback error: no observed attributes handler map object detected!');
+          throw new Error(
+            'GalExtendedCustomElement attributeChangedCallback error: no observed attributes handler map object detected!',
+          );
         }
-    
-        const attributeChangeHandler = (attributesMap as Partial<Record<keyof T, (from: string, to: string) => void>>)[observedAttributesMap[name]];
-    
+
+        const attributeChangeHandler = (attributesMap as Partial<
+          Record<keyof T, (from: string, to: string) => void>
+        >)[observedAttributesMap[name]];
+
         if (attributeChangeHandler) {
           attributeChangeHandler(from, to);
         }
@@ -268,8 +305,12 @@ export function GalExtendedCustomElement<T>(galExtendedCustomElement: IGalExtend
       }
     };
 
-    customElements.define(galExtendedCustomElement.is, galExtendedCustomElementDefintion, { extends: galExtendedCustomElement.extends });
+    customElements.define(
+      galExtendedCustomElement.is,
+      galExtendedCustomElementDefintion,
+      { extends: galExtendedCustomElement.extends },
+    );
 
     return galExtendedCustomElementDefintion;
-  }
+  };
 }
